365e1c0e09140bd3ac98466fadf815e46e390780
McKenney Leavell <maleavell@gmail.com>
2025-10-22 15:01:42 -0500

    edit post function working

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,3 @@
-from .posts import get_posts, get_post_by_id, delete_post
+from .posts import get_posts, get_post_by_id, delete_post, update_post
 from .user import create_user, login_user
 from .categories import get_categories
