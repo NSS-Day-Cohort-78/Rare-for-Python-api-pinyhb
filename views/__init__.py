c5c22947a5e0b6961a36f02a00445b45f5ac2255
McKenney Leavell <maleavell@gmail.com>
2025-10-23 11:47:18 -0500

    Get category by id added

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,3 @@
 from .posts import get_posts, get_post_by_id, delete_post, update_post
 from .user import create_user, login_user, get_user, get_all_users
-from .categories import get_categories, create_category, delete_category
+from .categories import get_categories, create_category, delete_category, get_category_by_id
