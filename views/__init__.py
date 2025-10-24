e471a42642bc4b934216b95f1c103f09efbf8b48
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-10-24 13:38:52 -0500

    get request for all tags

--- views/__init__.py
+++ views/__init__.py
@@ -9,3 +9,4 @@ from .categories import (
 )
 from .comments import get_all_comments, create_comment, edit_comment, get_comment_by_id
 from .reactions import get_post_reactions, create_reaction, get_all_reactions
+from .tags import get_all_tags
---
+++ views/tags.py
@@ -0,0 +1,27 @@
+import sqlite3
+import json
+
+db = "db.sqlite3"
+
+
+def get_all_tags():
+    """get all tags"""
+    with sqlite3.connect(db) as conn:
+        conn.row_factory = sqlite3.Row
+
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            SELECT * FROM Tags
+            """
+        )
+
+        response = cursor.fetchall()
+
+        tags = []
+        for row in response:
+            tags.append(dict(row))
+
+        serialized_tags = json.dumps(tags)
+    return serialized_tags
