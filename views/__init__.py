547d343093393868702178fc7361c68a7daeb2fe
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-10-27 13:08:51 -0500

    Added import

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,18 @@
 from .posts import get_posts, get_post_by_id, delete_post, update_post, create_post
 from .user import create_user, login_user, get_user, get_all_users
-from .categories import get_categories, create_category, delete_category
+from .categories import (
+    get_categories,
+    create_category,
+    delete_category,
+    get_category_by_id,
+    update_category,
+)
+from .comments import (
+    get_all_comments,
+    create_comment,
+    edit_comment,
+    get_comment_by_id,
+    delete_comment
+)
+from .reactions import get_post_reactions, create_reaction, get_all_reactions, create_post_reaction
+from .tags import get_all_tags, get_tag_by_id, update_tag
